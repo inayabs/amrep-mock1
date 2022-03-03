4f86ed7b2d71e0d367bd1e1de62036b33585bf23
inayabs <tsitonggwapo@gmail.com>
2022-03-04 02:20:50 +0800

    slider fix, landing button, mobile nav

--- components/image-slider.js
+++ components/image-slider.js
@@ -20,6 +20,9 @@ const ImageSlider = ({file}) => {
         }
         });
     }
+    const handleTouchMove = (event = TouchEvent) => {
+        slide(event.touches.item(0).clientX);
+      };
     const handleMouseDown = ()=>{
         window.onmousemove = handleMouseMove;
         window.onmouseup = handleMouseUp;
@@ -42,7 +45,27 @@ const ImageSlider = ({file}) => {
                 <div style={{left: `${imageRevealFraq * 100}%`}} className="absolute inset-y-0">
                     <div className="relative h-full">
                         <div className="absolute inset-y-0 bg-white w-0.5 -ml-px opacity-50"></div>
-                        <div onMouseDown={handleMouseDown} className="h-12 w-12 rounded-full bg-white -ml-6 -mt-6 absolute top-1/2"></div>
+                        <div
+                            style={{ touchAction: "none" }}
+                            onMouseDown={handleMouseDown}
+                            onTouchMove={handleTouchMove}
+                            className="h-12 w-12 -ml-6 -mt-6 rounded-full bg-white absolute top-1/2 shadow-xl flex items-center justify-center"
+                            >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="w-6 tex-gray-400 rotate-90 transform cursor-pointer"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M8 9l4-4 4 4m0 6l-4 4-4-4"
+                            />
+                        </svg>
+                        </div>
                     </div>
                 </div>
         </div>
--- components/layouts/landing.js
+++ components/layouts/landing.js
@@ -45,7 +45,7 @@ const index = () => {
               />
               <button
                 type="submit"
-                className="mt-3 w-full px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 shadow-sm hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:flex-shrink-0 sm:inline-flex sm:items-center sm:w-auto"
+                className="mt-3 w-full px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-800 shadow-sm hover:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:flex-shrink-0 sm:inline-flex sm:items-center sm:w-auto"
               >
                 Notify me
               </button>
